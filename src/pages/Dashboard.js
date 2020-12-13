9138ddec653c8a00d64710b7e2c729a9501c8ce7
ChristianMMason <christian@cmason.io>
2020-12-12 23:10:00 -0500

    Added Card component, added theme to card styles

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+  CardWrapper,
+  CardHeader,
+  CardHeading,
+  CardBody,
+} from "../components/Card";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <CardWrapper>
+        <CardHeader>
+          <CardHeading>ACFT Status</CardHeading>
+        </CardHeader>
+
+        <CardBody></CardBody>
+      </CardWrapper>
+    </div>
+  );
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -18,6 +18,8 @@ export const GlobalStyle = createGlobalStyle`
 
 export const Theme = {
   primary: "#ffd530",
+  fontFamily: "'Overpass', sans-serif",
+  textColor: "#121212",
   boxShadows: [
     "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
     "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
